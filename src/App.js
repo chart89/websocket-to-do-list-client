486ae5d36b8f2d22f65c7d047f62d4ee21222c96
Darek Biernat <kaiju83@outlook.com>
2023-09-24 22:50:24 +0200

    Add functions to update, remove and add tasks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
+import shortid from 'shortid';
+
 
 const App = () => {
 
   const [socket, setSocket] = useState();
-  const [tasks, setTasks] = useState([
-    {id: 1, taskName:'go work'},
-    {id: 2, taskName:'go gym'},
-    {id: 3, taskName:'go for a walk'},
-    {id: 4, taskName:'make sex'}]);
-
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState([]);
+  
   useEffect(() => {
     const socket = io('ws://localhost:8000', { transports: ['websocket'] });
     setSocket(socket);
+    socket.on('updateData', task => setTasks(task));
+    socket.on('addTask', incommingTask => addTask(incommingTask));
 
     return () => {
       socket.disconnect();
     };
   }, []);
 
-  const removeTask = (idOfTask) => {
-    for(let singleTask of tasks) {
-      if(singleTask.id === idOfTask) {
-        console.log('zostanie usunięty', singleTask)
-      }
-    }
-  }
+  const removeTask = (idTask) => {
+    setTasks(tasks => tasks.filter(task => task.id !== idTask));
+    socket.emit('removeTask', idTask);
+  };
+
+  const addTask = (task) => {
+    setTasks(tasks => [...tasks, task ]);
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+    const id = shortid();
+    addTask({name: taskName, id: id});
+    socket.emit('addTask', {name: taskName, id: id})
+    setTaskName('');
+  };
+
 
   return (
     <div className="App">
@@ -38,11 +49,11 @@ const App = () => {
         <h2>Tasks</h2>
   
         <ul className="tasks-section__list" id="tasks-list">
-          {tasks.map(task => {return <li className="task">{task.taskName} <button onClick={()=>removeTask(task.id)} className="btn btn--red">Remove</button></li>})}
+          {tasks.map(task => {return <li key={shortid()} className="task">{task.name} <button onClick={()=>removeTask(task.id)} className="btn btn--red">Remove</button></li>})}
         </ul>
   
-        <form id="add-task-form">
-          <input className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name" />
+        <form id="add-task-form" onSubmit={submitForm}>
+          <input className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name" value={taskName} onChange={e => setTaskName(e.target.value)} />
           <button className="btn" type="submit">Add</button>
         </form>
   
